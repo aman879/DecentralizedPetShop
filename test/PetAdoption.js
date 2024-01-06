05a69254ba612833cab30dae8cb5f0a2100cfb4c
Aman kumar <aman9693kumar@gmail.com>
2024-01-06 12:31:15 +0530

    Add Hardhat network configuration. (generated by blackbox.ai )

---
+++ test/PetAdoption.js
@@ -0,0 +1,10 @@
+
+const { expect } = require("chai");
+
+describe("PetAdoption", function () {
+    describe("Deployement", function() {
+        it("Should set the right owner", function() {
+            expect(true).to.equal(true);
+        })
+    })
+})
