e1976c55f20dd6e937db60914b39fc8a7c770ca6
Aman kumar <aman9693kumar@gmail.com>
2024-01-07 17:33:43 +0530

    initial frontend

--- contracts/PetAdoption.sol
+++ contracts/PetAdoption.sol
@@ -4,9 +4,53 @@
 pragma solidity ^0.8.9;
 
 contract PetAdoption {
-    address public owner;
+    address private owner;
+    uint public petIndex = 0;    //Pet number
+    uint[] private allAdoptedPets;
 
-    constructor() {
+    mapping(uint => address) private petIdToOwnerAddress;
+    mapping(address => uint[]) private ownerAddressToPetList;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can use this function");
+        _;
+    }
+
+    constructor(uint initialPetIndex) {
         owner = msg.sender;
+        petIndex = initialPetIndex;
+    }
+
+    function addPet() public onlyOwner() {
+        petIndex++;
+    }
+
+    function adoptPet(uint adpotId) public {
+        require(adpotId < petIndex, "Pet index out of bound");
+        require(petIdToOwnerAddress[adpotId] == address(0), "Pet is already adopted");
+
+        petIdToOwnerAddress[adpotId] = msg.sender;
+        ownerAddressToPetList[msg.sender].push(adpotId);
+        allAdoptedPets.push(adpotId);
+    }
+
+    function getOwner() public view returns(address) {
+        return owner;
+    }
+
+    function getPetOwner(uint id) public view returns(address) {
+        return petIdToOwnerAddress[id];
+    }
+
+    function getAllAdoptedPetsByOwner() public view returns(uint[] memory) {
+        return ownerAddressToPetList[msg.sender];
+    }
+
+    function getAllAdoptedPets() public view returns(uint[] memory) {
+        return allAdoptedPets;
+    }
+
+    function getAdoptedpetByIndex(uint id) public view returns(uint) {
+        return allAdoptedPets[id];
     }
 }
--- test/PetAdoption.js
+++ test/PetAdoption.js
@@ -1,10 +1,121 @@
 
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("PetAdoption", function () {
+    async function deployContractFixture() {
+        const PETS_COUNT = 5
+        const ADOPT_INDEX = 0;
+        const [owner, account2, account3 ] = await ethers.getSigners()
+        const PetAdoption = await ethers.getContractFactory("PetAdoption")
+        const contract = await PetAdoption.deploy(PETS_COUNT)
+
+        await contract.connect(account3).adoptPet(ADOPT_INDEX)
+
+        return { 
+            owner,
+            contract,
+            account2, 
+            account3,
+            petsAddedCount: PETS_COUNT,
+            adoptPetId: ADOPT_INDEX
+        }
+    }
+
     describe("Deployement", function() {
-        it("Should set the right owner", function() {
-            expect(true).to.equal(true);
+        it("Should return the right owner", async function() {
+            const { owner, contract } = await loadFixture(deployContractFixture)
+
+            expect(await contract.getOwner()).to.equal(owner.address)
+        })
+    })
+
+    describe("Add pet", function() {
+        it("Should revert with only owner error if called from another accounts", async function() {
+            const { owner, contract, account2 } = await loadFixture(deployContractFixture)
+
+            await expect(contract.connect(account2).addPet()).to.be.revertedWith("Only owner can use this function")
+        })
+
+        it("Should increase pet index", async function() {
+            const { contract, petsAddedCount } =  await loadFixture(deployContractFixture)
+
+            await contract.addPet()
+            
+            expect(await contract.petIndex()).to.equal(petsAddedCount + 1)
+        })
+    })
+
+    describe("Adopt Pet", function() {
+        it("Should revert with index out of bound", async function() {
+            const { contract, petsAddedCount } = await loadFixture(deployContractFixture)
+
+            await expect(contract.adoptPet(petsAddedCount)).to.be.revertedWith("Pet index out of bound")
+        })
+
+        it("Should revert with Pet already adopted", async function() {
+            const { contract, adoptPetId } = await loadFixture(deployContractFixture)
+            
+            await expect(contract.adoptPet(adoptPetId)).to.be.revertedWith("Pet is already adopted")
+        })
+
+        it("Should succesfuly adopt pet", async function() {
+            const { contract, account2} = await loadFixture(deployContractFixture)
+
+            await expect(contract.connect(account2).adoptPet(1)).not.to.be.reverted
+        })
+
+        it("Should link pet ID with owner address", async function () {
+            const { contract, account3 } = await loadFixture(deployContractFixture)
+
+            expect(await contract.getPetOwner(0)).to.equal(account3.address)
+        })
+
+        it("Should push pet list to its owner array", async function() {
+            const { contract, account2 } = await loadFixture(deployContractFixture)
+            const firstPetId = 1
+            const secondPetId = 2
+
+            
+            await contract.connect(account2).adoptPet(firstPetId)
+            await contract.connect(account2).adoptPet(secondPetId)
+
+            const petsByOwner = await contract.connect(account2).getAllAdoptedPetsByOwner(account2)
+
+            expect(petsByOwner.length).to.equal(2)
+        })
+
+        it("should add adopted pets in adopted list", async function() {
+            const { contract, account2 } = await loadFixture(deployContractFixture)
+
+            const petId = 1
+
+            await contract.connect(account2).adoptPet(petId)
+            const allPet = await contract.getAllAdoptedPets()
+
+            expect(allPet.length).to.equal(2)
+        })
+
+        it("Should give correct adopted pet", async function() {
+            const { contract } = await loadFixture(deployContractFixture)
+
+            const petId = 1
+
+            await contract.adoptPet(petId)
+
+            expect(await contract.getAdoptedpetByIndex(1)).to.equal(petId)
+        })
+
+        it("should return zero address for pet which are not adopted", async function() {
+            const { contract } = await loadFixture(deployContractFixture)
+
+            const zeroAddress = await contract.getPetOwner(100)
+
+            expect(zeroAddress).to.equal("0x0000000000000000000000000000000000000000")
         })
     })
 })
+
+
+// npx hardhat test --network networkName
